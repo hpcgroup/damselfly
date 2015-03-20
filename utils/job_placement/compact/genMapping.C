8e5d0db5f225c81277a248cf1e285b11f781dfcd
Abhinav Bhatele <bhatele@llnl.gov>
2015-03-19 23:14:38 -0700

    match variable names to randomMapping generator

--- utils/job_placement/compact/genMapping.C
+++ utils/job_placement/compact/genMapping.C
@@ -2,20 +2,20 @@
 #include <cstdlib>
 #include <string.h>
 
-int numgroups, numchassis, numrouters, numpcis, numcores;
+int numgroups, numrows, numcols, numnodesperrouter, numcores;
 
 void rankToCoords(int rank, int *dims) {
 	dims[4] = rank % numcores;
 	rank /= numcores;
 
-	dims[3] = rank % numpcis;
-	rank /= numpcis;
+	dims[3] = rank % numnodesperrouter;
+	rank /= numnodesperrouter;
 
-	dims[2] = rank % numrouters;
-	rank /= numrouters;
+	dims[2] = rank % numcols;
+	rank /= numcols;
 
-	dims[1] = rank % numchassis;
-	rank /= numchassis;
+	dims[1] = rank % numrows;
+	rank /= numrows;
 
 	dims[0] = rank % numgroups;
 }
@@ -23,17 +23,17 @@ void rankToCoords(int rank, int *dims) {
 int main(int argc, char**argv) {
 
   numgroups = atoi(argv[1]);
-  numchassis = atoi(argv[2]);
-  numrouters = atoi(argv[3]);
-  numpcis = atoi(argv[4]);
+  numrows = atoi(argv[2]);
+  numcols = atoi(argv[3]);
+  numnodesperrouter = atoi(argv[4]);
   numcores = atoi(argv[5]);
   int jobsize = atoi(argv[6]);
 
   int dims[5];
   int target = 0;
-  for(int currentGroup = 0; currentGroup < (jobsize/(numchassis*numrouters*numpcis*numcores)); currentGroup++) {
-  	target = currentGroup*numchassis*numrouters*numpcis*numcores;
-  	for(int i = 0; i < (numchassis*numrouters*numpcis*numcores); i++) {
+  for(int currentGroup = 0; currentGroup < (jobsize/(numrows*numcols*numnodesperrouter*numcores)); currentGroup++) {
+  	target = currentGroup*numrows*numcols*numnodesperrouter*numcores;
+  	for(int i = 0; i < (numrows*numcols*numnodesperrouter*numcores); i++) {
   		rankToCoords(target, dims);
   		for(int j = 0; j < 5; j++) {
 	  		printf("%d ", dims[j]);
