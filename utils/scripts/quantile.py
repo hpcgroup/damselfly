79925e38fc530a1eb0f9ee1a6d2b042bd032821f
Abhinav Bhatele <bhatele@llnl.gov>
2015-04-15 02:04:19 -0700

    add glob functionality

--- utils/scripts/quantile.py
+++ utils/scripts/quantile.py
@@ -24,16 +24,17 @@ import matplotlib.pyplot as pl
 if __name__ == '__main__':
   from sys import argv
 
-  num_jobs = int(argv[2])
-  color = argv[3]
+  num_jobs = int(argv[1])
+  color = argv[2]
 
-  (alllinks, green, black, blue)  = readfile(argv[1], num_jobs)
+  for filename in glob.glob('single/*/links-*.csv'):
+    (alllinks, green, black, blue) = readfile(filename, num_jobs)
 
-  if color == 'total':
-    print "%0.3f %0.3f %0.3f" % (np.percentile(alllinks, 0), np.average(alllinks), np.percentile(alllinks, 100))
-  if color == 'green':
-    print "%0.3f %0.3f %0.3f" % (np.percentile(green, 0), np.average(green), np.percentile(green, 100))
-  if color == 'black':
-    print "%0.3f %0.3f %0.3f" % (np.percentile(black, 0), np.average(black), np.percentile(black, 100))
-  if color == 'blue':
-    print "%0.3f %0.3f %0.3f" % (np.percentile(blue, 0), np.average(blue), np.percentile(blue, 100))
+    if color == 'total':
+      print "%s %0.3f %0.3f %0.3f" % (filename, np.percentile(alllinks, 0), np.average(alllinks), np.percentile(alllinks, 100))
+    if color == 'green':
+      print "%0.3f %0.3f %0.3f" % (np.percentile(green, 0), np.average(green), np.percentile(green, 100))
+    if color == 'black':
+      print "%0.3f %0.3f %0.3f" % (np.percentile(black, 0), np.average(black), np.percentile(black, 100))
+    if color == 'blue':
+      print "%0.3f %0.3f %0.3f" % (np.percentile(blue, 0), np.average(blue), np.percentile(blue, 100))
