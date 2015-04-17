5034e25b80b18976bf0cb801df1451f16db4fad2
Abhinav Bhatele <bhatele@llnl.gov>
2015-04-16 18:03:44 -0700

    write out all files at once

--- utils/scripts/quantile.py
+++ utils/scripts/quantile.py
@@ -24,22 +24,28 @@ import matplotlib.pyplot as pl
 if __name__ == '__main__':
   from sys import argv
 
-  color = argv[1]
+  fileprefix = argv[1]
 
-  filelist = sorted(glob.glob('single/*/links-*.csv'))
-  # for i in range(0,14):
-  #   temp = filelist[5*i]
-  #   filelist[5*i] = filelist[5*i+4]
-  #   filelist[5*i+4] = temp
+  filelist = sorted(glob.glob('multi/4jobs-1/*/links-*.csv'))
+  # for i in range(0,5):
+  #     temp = filelist[5*i]
+  #     filelist[5*i] = filelist[5*i+4]
+  #     filelist[5*i+4] = temp
+
+  file1 = open("%s-total.dat" % fileprefix, "w")
+  file2 = open("%s-green.dat" % fileprefix, "w")
+  file3 = open("%s-black.dat" % fileprefix, "w")
+  file4 = open("%s-blue.dat" % fileprefix, "w")
 
   for filename in filelist:
-    (alllinks, green, black, blue) = readfile(filename)
+      (alllinks, green, black, blue) = readfile(filename)
 
-    if color == 'total':
-      print "%s %0.3f %0.3f %0.3f" % (filename, np.percentile(alllinks, 0), np.average(alllinks), np.percentile(alllinks, 100))
-    if color == 'green':
-      print "%s %0.3f %0.3f %0.3f" % (filename, np.percentile(green, 0), np.average(green), np.percentile(green, 100))
-    if color == 'black':
-      print "%s %0.3f %0.3f %0.3f" % (filename, np.percentile(black, 0), np.average(black), np.percentile(black, 100))
-    if color == 'blue':
-      print "%s %0.3f %0.3f %0.3f" % (filename, np.percentile(blue, 0), np.average(blue), np.percentile(blue, 100))
+      file1.write("%s %0.3f %0.3f %0.3f\n" % (filename, np.percentile(alllinks, 0), np.average(alllinks), np.percentile(alllinks, 100)))
+      file2.write("%s %0.3f %0.3f %0.3f\n" % (filename, np.percentile(green, 0), np.average(green), np.percentile(green, 100)))
+      file3.write("%s %0.3f %0.3f %0.3f\n" % (filename, np.percentile(black, 0), np.average(black), np.percentile(black, 100)))
+      file4.write("%s %0.3f %0.3f %0.3f\n" % (filename, np.percentile(blue, 0), np.average(blue), np.percentile(blue, 100)))
+
+file1.close()
+file2.close()
+file3.close()
+file4.close()
