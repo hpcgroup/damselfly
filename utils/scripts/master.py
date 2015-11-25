24a757c31b8e1822ca540212ff6b8f4396f2be32
Abhinav Bhatele <bhatele@llnl.gov>
2015-11-24 18:19:50 -0800

    add a script to generate a multi-sim comparison file

---
+++ utils/scripts/master.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import glob
+import numpy as np
+import re
+from loaddata import *
+
+if __name__ == '__main__':
+  from sys import argv
+
+  netconfigs = ['default', 'M1K', 'M1KP1B', 'M1KP3B', 'P1B', 'P3B']
+  datasets = ['4jobs-1', '4jobs-2', '8jobs-1', '8jobs-2']
+
+  print "config,dataset,sim,jobid,color,min,avg,max,nonzero,nzavg"
+
+  for nc in netconfigs:
+    for ds in datasets:
+      filelist = sorted(glob.glob(nc + '/' + ds + '/sim*/links*k[48].csv') + glob.glob(nc + '/' + ds + '/sim*/links*k.csv'))
+
+      for filename in filelist:
+	simid = re.search('sim\d+', filename).group(0)
+
+	(numjobs, alllinks, green, black, blue) = readfile(filename, -1)
+	for job in range(numjobs):
+	  (numjobs, alllinks, green, black, blue) = readfile(filename, job)
+
+	  print "%s,%s,%s,%d,%d,%0.3f,%0.3f,%0.3f,%d,%0.3f" % (nc, ds, simid, job, 0, np.amin(alllinks), np.average(alllinks), np.amax(alllinks), np.count_nonzero(alllinks), np.sum(alllinks)/np.count_nonzero(alllinks))
+	  print "%s,%s,%s,%d,%d,%0.3f,%0.3f,%0.3f,%d,%0.3f" % (nc, ds, simid, job, 1, np.amin(green), np.average(green), np.amax(green), np.count_nonzero(green), np.sum(green)/np.count_nonzero(green))
+	  print "%s,%s,%s,%d,%d,%0.3f,%0.3f,%0.3f,%d,%0.3f" % (nc, ds, simid, job, 2, np.amin(black), np.average(black), np.amax(black), np.count_nonzero(black), np.sum(black)/np.count_nonzero(black))
+	  print "%s,%s,%s,%d,%d,%0.3f,%0.3f,%0.3f,%d,%0.3f" % (nc, ds, simid, job, 3, np.amin(blue), np.average(blue), np.amax(blue), np.count_nonzero(blue), np.sum(blue)/np.count_nonzero(blue))
+
